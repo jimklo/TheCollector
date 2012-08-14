751062e1fea6a95e89482cc5b5be8b412ae6819f
Jim Klo <jim.klo@sri.com>
2012-08-14 16:24:29 -0700

    fix zip format

--- tools/pack.sh
+++ tools/pack.sh
@@ -8,3 +8,9 @@ find ../thecollector-pack \
     -iregex '.*\.js$' -and -not -name '*min*' -and -not -name '*pack*' \
     -exec java -jar ./compiler-latest/compiler.jar --js '{}' --js_output_file '{}-compressed' \; \
     -exec mv -fv '{}-compressed' '{}' \;
+
+pushd ../
+
+zip -r thecollector-pack`date +%s`.zip thecollector-pack
+
+popd
